803b96a2f28d77cc4be41b1c6f1e8e39d5d2a16e
Vadzim Vysotski <vadzimv@playtika.com>
2018-07-06 17:51:16 +0300

    added full or empty test which fails

--- ring_buffer/test/RingBufferTest.cpp
+++ ring_buffer/test/RingBufferTest.cpp
@@ -90,6 +90,20 @@ TEST(RingBuffer, cycleWriteRead) {
     ASSERT_STREQ(buffer, "45678");
 }
 
+TEST(RingBuffer, writeUntillFullThenReadAllThenWriteAgain) {
+    char *data = "12345";
+    RingBuffer rb(4);
+    char buffer[6] = {};
+
+    rb.write(data, 5);
+    rb.read(buffer, 5);
+
+    rb.write("6789", 4);
+    rb.read(buffer, 4);
+    
+    ASSERT_STREQ(buffer, "6789");
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
